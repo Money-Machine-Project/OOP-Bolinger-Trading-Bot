10255668ee914447e520ea367338662ba7918e47
seungryeol <opop0421@naver.com>
2025-03-12 14:03:56 +0900

    [feat] 조건 코딩

--- src/CutCondition.ts
+++ src/CutCondition.ts
@@ -1,9 +1,27 @@
+import { getValue } from "./db/redisManager.js";
+import isTradingAllowed from "./util/isTradeAllowed.js";
+
 interface CutCondition {
   evaluate(): boolean | Promise<boolean>;
 }
 
 class NPlusCutCondition implements CutCondition {
-  evaluate(): boolean | Promise<boolean> {
+  constructor(
+    private sellPrice: number,
+    private price: number,
+    private data: any
+  ) {}
+  async evaluate(): Promise<boolean> {
+    const canCutOff = await isTradingAllowed("cut");
+    if (
+      canCutOff &&
+      this.sellPrice !== null &&
+      this.sellPrice > this.price &&
+      Number(this.data.output1[0].hldg_qty) ===
+        Number(await getValue("buyCount"))
+    ) {
+      return true;
+    }
     return false;
   }
 }
--- src/TradingCondition.ts
+++ src/TradingCondition.ts
@@ -1,10 +1,16 @@
+import isTradingAllowed from "./util/isTradeAllowed.js";
+
 interface TradingCondition {
   evaluate(): boolean | Promise<boolean>;
 }
 
 class NPlusTradingCondition implements TradingCondition {
-  evaluate(): boolean | Promise<boolean> {
-    return false;
+  async evaluate(): Promise<boolean> {
+    const [canSell, canBuy] = await Promise.all([
+      isTradingAllowed("sell"),
+      isTradingAllowed("buy"),
+    ]);
+    return canSell || canBuy;
   }
 }
 
