465e594e726c477c42947b08b7b90fe04bd9d504
seungryeol <opop0421@naver.com>
2025-03-12 13:58:06 +0900

    NoTradingCondition 구현

--- src/NoTradingCondition.ts
+++ src/NoTradingCondition.ts
@@ -1,9 +1,20 @@
+import checkTradingTimeExceed from "./util/checkTradingTimeExceed.js";
+
 interface NoTradingCondition {
   evaluate(): boolean | Promise<boolean>;
 }
 
 class NPlusNoTradingCondition implements NoTradingCondition {
+  constructor(private tradingTime: string) {}
   evaluate(): boolean | Promise<boolean> {
+    if (
+      this.tradingTime !== null &&
+      (this.tradingTime.split("+")[1] === "sell" ||
+        this.tradingTime.split("+")[1] === "buy") &&
+      checkTradingTimeExceed(this.tradingTime.split("+")[0])
+    ) {
+      return true;
+    }
     return false;
   }
 }
